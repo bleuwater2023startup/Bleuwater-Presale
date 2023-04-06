752b2ae4950984cc5aea256e5194bd77ea4194a9
EECvision <eec.studies@gmail.com>
2023-04-06 18:10:38 +0100

    update feedback

--- components/Home/FAQ/FAQData.js
+++ components/Home/FAQ/FAQData.js
@@ -1,31 +1,26 @@
 export const data = [
   {
-    name: "What is Bleuwaters?",
-    description:
-      "Bleuwaters empowers brands, creators to have their NFT ecosystem with scalable smart contracts that protect their NFT and ensure they get royalties each time their NFT sells on the secondary market. We also educate and  onboards new and existing creators, artists, brands/businesses to the new economic opportunities in the Web3.",
-  },
-  {
     name: "What Blockchain does Bleuwater support?",
     description:
-      "Currently we only deployed to  Etherium blockchian, and still building on polygon, Aptos, Tezos and Solana.",
+      "Currently we only deployed to  Ethereum and polygon blockchain.  We’re still building on Solana and Tezos.",
   },
   {
     name: "Where can I buy and sell Bleuwater NFTs",
     description: "You can only buy and trade Bleuwaters NFTs on Bleuwater.com",
   },
   {
-    name: "Can I invest?",
-    description:
-      "We are currently selling our limited NFT collection on Ethereum Blockchain. If you are looking to invest you can buy into our investor NFT tier. Holding more of our NFT gives you access to different perks and platform-based benefits as we move to onboard more users to web3.",
-  },
-  {
     name: "What Utility does the Bleuwaters NFT have?",
     description:
-      "Holding our NFTs gives you exclusive access to our E-learning classes, access to our IRL events, an hour One-on-one call with the team, and some merch.",
+      "Holding our NFTs gives you exclusive access to our E-learning classes, Access to our IRL events, a 1hr One-on-one call with the team, and building your own eco system.",
   },
   {
-    name: "How is Royalty paid?",
+    name: "Can I transfer my NFT to other wallet?",
     description:
-      "Royalties are automatically paid out to wallet addresses once a sale is completed.",
+      "Yes, however you can only do that from our website using the transfer button from the NFT detail page.",
+  },
+  {
+    name: "Will I loose access to my NFT utilities once I transfer them?",
+    description:
+      "No, you are not going to loose access to your NFT utilities once transferred  to  another wallet. The previous state of the utilities are carried over to the new holder, except on point of sale.",
   },
 ];
